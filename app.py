75618553da4e6e96eff29f602a1b247b078d7319
jatingangare44 <jating22it@student.mes.ac.i>
2025-01-18 22:00:44 +0530

    Commit

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ import pickle
 import numpy as np
 
 # Load the model and scaler
-with open('runtime_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
-with open('scaler.pkl', 'rb') as scaler_file:
-    scaler = pickle.load(scaler_file)
+model = pickle.load(open('runtime_model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
 
 # Streamlit app header
 st.title("Movie Runtime Prediction")
